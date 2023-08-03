34fb35adfb962b73cadda23bb95caf59bae50e46
AyaanDanish <ayaandanish7@gmail.com>
2023-08-03 16:14:42 +0000

    fix:script-param-type-mismatch

--- element.sh
+++ element.sh
@@ -6,15 +6,20 @@ if [[ ! $1 ]]
 then
   echo Please provide an element as an argument.
 else
-  ATOMIC_NUM=$($PSQL "select atomic_number from elements where atomic_number = $1 or symbol = '$1' or name = '$1';")
+  if [[ $1 =~ [0-9]+ ]] 
+  then
+    ATOMIC_NUM=$($PSQL "select atomic_number from elements where atomic_number = $1")
+  else
+    ATOMIC_NUM=$($PSQL "select atomic_number from elements where symbol = '$1' or name = '$1';")
+  fi
   if [[ -z $ATOMIC_NUM ]]
   then
     echo "I could not find that element in the database."
   else
-    ELEMENT_PROPS="$($PSQL "select atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties using(atomic_number) where atomic_number=1;")"
-    echo $ELEMENT_PROPS | while IFS="|" read ATOMIC SYMBOL NAME TYPE MASS MELTING BOILING
+    ELEMENT_PROPS="$($PSQL "select atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties using(atomic_number) where atomic_number = $ATOMIC_NUM")"
+    echo $ELEMENT_PROPS | while IFS="|" read ATOMIC_NUM SYMBOL NAME TYPE MASS MELTING BOILING
     do
-      echo $ATOMIC $SYMBOL $NAME $TYPE $MASS $MELTING $BOILING
+      echo "The element with atomic number $ATOMIC_NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
     done
   fi
 fi

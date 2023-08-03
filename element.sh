c7f46c5313aef20f609f05b29715b980bdf9ed71
AyaanDanish <ayaandanish7@gmail.com>
2023-08-03 16:02:31 +0000

    fix:destructuring-element-props

--- element.sh
+++ element.sh
@@ -11,10 +11,10 @@ else
   then
     echo "I could not find that element in the database."
   else
-    ELEMENT_PROPS=$($PSQL "select atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties using(atomic_number) where atomic_number=1;")
-    echo $ELEMENT_PROPS | while read ATOMIC_NUM BAR SYMBOL BAR NAME BAR TYPE BAR MASS BAR MELTING BAR BOILING
+    ELEMENT_PROPS="$($PSQL "select atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties using(atomic_number) where atomic_number=1;")"
+    echo $ELEMENT_PROPS | while IFS="|" read ATOMIC SYMBOL NAME TYPE MASS MELTING BOILING
     do
-      echo $ATOMIC_NUM $SYMBOL $NAME $TYPE $MASS $MELTING $BOILING 
+      echo $ATOMIC $SYMBOL $NAME $TYPE $MASS $MELTING $BOILING
     done
   fi
 fi
